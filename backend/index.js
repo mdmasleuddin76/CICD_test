86f2307c1e78fc40b083a6d27da4ecc870b9a4cc
Md Masleuddin <100273014+mdmasleuddin76@users.noreply.github.com>
2024-07-17 13:44:22 +0530

    first

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+// require('dotenv').config()
+import "dotenv/config";
+import express from "express";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+const app = express();
+import cors from "cors";
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`);
+});
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+async function run(test) {
+
+  const prompt = `Please generate a list of 5 movies similar to Money ${test}. Provide the movies in a comma-separated format with each movie's name and genre. The format should be exactly as follows: name:Movie Name, genre:Genre. Ensure there are no extra characters, no additional text, and no colons (:) used anywhere except after name and genre.example, name:Money Heist, genre:action,  name:The Wandering Earth, genre:sci-fi..Do not write anything else in the response. This is crucial for my code to parse the data correctly.if there are multiple genre for a movie, please separate them with a slash (/) and no space.`;
+
+  return prompt;
+}
+
+app.post("/", async (req, res) => {
+  const { test } = req.body;
+  const data = await run(test);
+  res.json({ name: "Hacked", data: data });
+});
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+import axios from "axios";
+import { useEffect,useState } from "react";
+export default function Home() {
+  const [data, setData] = useState({});
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:4000/");
+    setData(response.data);
+    console.log(response.data);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div className=" w-[70%] m-auto text-center">
+      <h1>Home</h1>
+      <p>Welcome to Home Page</p>
+      <p> Some data that has been feteched from backend are following</p>
+      <div className="bg-slate-50 my-7 p-7">
+        {
+          data? data.data:""
+        }
+      </div>
+      <p>i have added some content here at 6:34 </p>
+    </div>
+  );
+}
