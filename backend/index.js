014c103e07bdd9542470a3a09d8fca929032dd3f
Md Masleuddin <100273014+mdmasleuddin76@users.noreply.github.com>
2024-07-17 15:21:37 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
 });
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "http://frontend:3000" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
