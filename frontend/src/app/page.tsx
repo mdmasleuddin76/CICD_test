9b30b2be3ebd6d627a6b27a67e487f2b603cb2b4
Md Masleuddin <100273014+mdmasleuddin76@users.noreply.github.com>
2024-07-17 14:04:57 +0530

    second

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,19 +3,22 @@ import axios from "axios";
 import { useEffect,useState } from "react";
 export default function Home() {
   const [data, setData] = useState({});
+  const [input,setinput]=useState("");
   const fetchData = async () => {
-    const response = await axios.get("http://localhost:4000/");
+    const response = await axios.post("http://localhost:4000/",{test:input});
     setData(response.data);
     console.log(response.data);
   };
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, [input]);
   return (
     <div className=" w-[70%] m-auto text-center">
       <h1>Home</h1>
       <p>Welcome to Home Page</p>
       <p> Some data that has been feteched from backend are following</p>
+      <input type="text" onChange={(e)=>setinput(e.target.value)} className=" border border-red-600 p-2 m-4" />
+      <button className=" border border-sky-300 bg-slate-50 m-2 p-3 rounded-md" onClick={()=>fetchData()}>Fetch Data</button>
       <div className="bg-slate-50 my-7 p-7">
         {
           data? data.data:""
