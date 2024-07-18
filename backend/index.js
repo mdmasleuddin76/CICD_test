eb5ba7af724bc37610f38a2166f4379d5432378b
Md Masleuddin <100273014+mdmasleuddin76@users.noreply.github.com>
2024-07-18 16:46:35 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 async function run(test) {
 
-  const prompt = `Please generate a list of 5 movies similar to Money \n ${test}.\n Provide the movies in a comma-separated format with each movie's name and genre. The format should be exactly as follows: name:Movie Name, genre:Genre. Ensure there are no extra characters, no additional text, and no colons (:) used anywhere except after name and genre.example, name:Money Heist, genre:action,  name:The Wandering Earth, genre:sci-fi..Do not write anything else in the response. This is crucial for my code to parse the data correctly.if there are multiple genre for a movie, please separate them with a slash (/) and no space.`;
+  const prompt = `Please generate a list of 5 movies similar to Money \n ${test}.\n Provide the movies in a comma-separated format with each movie's name and genre. The format should be exactly as follows: name:Movie Name, genre:Genre. Ensure there are no extra characters, no additional text, and no colons (:) used anywhere except after name and genre.example, name:Money Heist, genre:action,kdlgjlgndgdl  name:The Wandering Earth, genre:sci-fi..Do not write anything else in the response. This is crucial for my code to parse the data correctly.if there are multiple genre for a movie, please separate them with a slash (/) and no space.`;
 
   return prompt;
 }
