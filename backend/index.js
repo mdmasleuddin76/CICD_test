36d8aa45a53ed6fec46bd76fc708838603a5cd87
Md Masleuddin <100273014+mdmasleuddin76@users.noreply.github.com>
2024-07-25 23:04:05 +0530

    up

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,8 @@ app.listen(port, () => {
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+const genAI = new GoogleGenerativeAI(AIzaSyAz_lPhTPQcudHRLeL9J9F2OoeRzqVz04g);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 async function run(test) {
   const prompt = `Please generate a list of 5 movies similar to Money \n ${test}.\n Provide the movies in a comma-separated format with each movie's name and genre. The format should be exactly as follows: name:Movie Name, genre:Genre. Ensure there are no extra characters, no additional text, and no colons (:) used anywhere except after name and genre.example, name:Money Heist, genre:action,kdlgjlgndgdl  name:The Wandering Earth, genre:sci-fi..Do not write anything else in the response. This is crucial for my code to parse the data correctly.if there are multiple genre for a movie, please separate them with a slash (/) and no space.`;
 
@@ -29,18 +30,22 @@ app.get("/", async (req, res) => {
   res.json({ name: "Hacked", data: data });
 });
 
-app.post("/", (req, res) => {
+app.post("/", async(req, res) => {
   const twiml = new MessagingResponse();
-
-  if (req.body.Body == "hello") {
-    twiml.message("Hi!");
-  } else if (req.body.Body == "bye") {
-    twiml.message("Goodbye");
-  } else {
-    twiml.message(
-      "No Body param match, Twilio sends this in the request to your server."
-    );
-  }
-
+  // const result = await model.generate(req.body.Body);
+  const result = await model.generateContent(req.body.Body);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+  twiml.message(text);
+  // if (req.body.Body == "hello") {
+  //   twiml.message("Hi!");
+  // } else if (req.body.Body == "bye") {
+  //   twiml.message("Goodbye");
+  // } else {
+  //   twiml.message(
+  //     "No Body param match, Twilio sends this in the request to your server."
+  //   );
+  // }
   res.type("text/xml").send(twiml.toString());
 });
